e9a59e561197a4475be569981f0ddff3a2662f44
gabiscardigno <79734824+gabiscardigno@users.noreply.github.com>
2021-03-09 21:31:50 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="container-fluid">
+        <!-- div for the main image to stretch s to the edge of the page-->
+
+        <div class="container">
+            <!-- div to hold the content in the middle of the page-->
+            <div class="row">
+                <!-- a row that gives us access to the BS columns-->
+                <div class="col-md-6 text-center footer-content ">
+                    <p><?php bloginfo('name'); ?></p>
+                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/facebook.jpg" alt="facebook">
+                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/linkedin.jpg" alt="linkedin">
+                </div>
+                <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p>
+                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/twitter.jpg" alt="twitter">
+                    <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/othersocial.jpg" alt="facebook">
+                </div><!-- row-->
+            </div><!-- container -->
+        </div><!-- container-fluid-->
+
+    </footer>
+
+
+</body>
+
+</html>
+<?php wp_footer (); ?>
