62d9eac7815e0ff107a108def691695e618361fd
gabiscardigno <79734824+gabiscardigno@users.noreply.github.com>
2021-03-19 16:03:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 </head>
 
 <body>
-
+    
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 
     <section class="container-fluid aboutbg text-center">
@@ -31,19 +31,16 @@
 <?php
 global $more;//define a global variable
 $more = 0;// the global varibale is now equal to 0
-query_posts('cat=2');//look for posts that have the category of 2
+query_posts('cat=22');//look for posts that have the category of 2
 if(have_posts()) ://if we have posts to display
 while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
 ?>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-            <div>
-                <p class="about-text"><?php the_content() ?></p>
-            </div>
-            <?php
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text"><?php the_content() ?></p></div>
+<?php
 endwhile;
 endif;
 wp_reset_query();?>
-
             <div class="blocker"></div>
         </div><!-- container-->
     </section>
@@ -82,4 +79,4 @@ wp_reset_query();?>
         </div>
     </section>
 
-    <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
